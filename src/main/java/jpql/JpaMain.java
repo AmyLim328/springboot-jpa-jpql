296aaf7c22cdbfe56e8e7131bfc1e36920fde45e
Amy Lim <amylim32897@gmail.com>
2022-03-29 16:41:23 +0900

    페치 조인 2 - 한계

--- src/main/java/jpql/JpaMain.java
+++ src/main/java/jpql/JpaMain.java
@@ -37,13 +37,16 @@ public class JpaMain {
             em.flush();
             em.clear();
 
-            String query = "select t from Team t join fetch t.members m";
-            List<Team> result = em.createQuery(query, Team.class).getResultList();
+            String query = "select t from Team t";
+            List<Team> result = em.createQuery(query, Team.class)
+                    .setFirstResult(0)
+                    .setMaxResults(2)
+                    .getResultList();
 
-            System.out.println("result = " + result.size());
+            System.out.println("result.size() = " + result.size());
 
             for (Team team : result) {
-                System.out.println("team= " + team.getName() + " | members= " + team.getMembers().size());
+                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
                 for (Member member : team.getMembers()) {
                     System.out.println("-> member = " + member);
                 }
